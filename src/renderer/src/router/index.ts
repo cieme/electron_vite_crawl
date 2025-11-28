7cc3e47bf609bf8ae00d815de12832f4e08a2b55
刘天成 <liuTianCheng@163.com>
2025-11-28 14:11:39 +0800

    feat:路由问题

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import HomeView from '@renderer/views/home/index.vue'
 const routes = [{ path: '/', component: HomeView }]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
 })
 
 export default router
